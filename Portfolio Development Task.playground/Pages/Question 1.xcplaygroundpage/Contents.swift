bb5fbd8e71e4d74c28312e108296a7354aaa1cb1
Matthew Amaral <mamaral@lcs-mba3204.lcs.on.ca>
2017-12-16 10:55:31 -0500

    Question 1 done

--- Portfolio Development Task.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -10,9 +10,9 @@
  Use the camel case convention for variable names.
  */
 // Answer question 1 below
-var howlongwetravelledfor = 5.875
-var s = 80.0
-let dct = s * howlongwetravelledfor
+var howLongWeTravelledFor = 5.875
+var speedOfTheCar = 80.0
+let howFarTheCarTravelled = speedOfTheCar * howLongWeTravelledFor
 
 /*:
 - callout(Shortcut): The **Edit All in Scope** menu command is useful when renaming variables – place your cursor within the variable name and then invoke the menu command.
